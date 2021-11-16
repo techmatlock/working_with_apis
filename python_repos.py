a39b23ff9eb41e01d5ef277486294175537098af
Mark Matlock <techmatlock@gmail.com>
2021-11-16 14:07:52 -0600

    Added spacing.

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,5 @@
 import requests
+
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
